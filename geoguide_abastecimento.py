29623b07296ad4364e9def98dbcde6209857bf85
RANDER FELIPE MIRANDA DE PAULA <rpaula@usangelo.com>
2025-05-27 08:08:06 -0300

    :bookmark: feat(main) Adicionando Estoque

--- geoguide_abastecimento.py
+++ geoguide_abastecimento.py
@@ -17,28 +17,51 @@ def get_connection():
 
 # Interface
 st.set_page_config(page_title="Consulta Geoguide", layout="wide")
-st.title("⛽ Consulta de Abastecimentos Geoguide")
+st.title("⛽ Consulta de Geoguide v2.0")
 
 # Configurações iniciais de sessão
 if "data_ini" not in st.session_state:
-    st.session_state["data_ini"] = date.today().replace(day=1)
+    st.session_state["data_ini"] = date.today()
 if "data_fim" not in st.session_state:
     st.session_state["data_fim"] = date.today()
 if "comboio" not in st.session_state:
     st.session_state["comboio"] = ""
+if "bomba" not in st.session_state:
+    st.session_state["bomba"] = ""
+
+st.sidebar.header("🧭 Fonte de Dados")
+fonte_dados = st.sidebar.selectbox("Escolha a origem dos dados:", options=["Abastecimento", "Estoque"])
 
 # Filtros
 with st.sidebar:
     st.header("📅 Filtros")
 
-    data_ini = st.date_input("Data Inicial", value=date.today().replace(day=1))
-    data_fim = st.date_input("Data Final", value=date.today())
-    comboio = st.text_input("Comboio (Opcional)", value=st.session_state["comboio"])
+    data_ini = st.date_input("Data Inicial", value=st.session_state["data_ini"])
+    data_fim = st.date_input("Data Final", value=st.session_state["data_fim"])
+
+    if fonte_dados == "Abastecimento":
+        comboio = st.text_input("Comboio (Opcional)", value=st.session_state["comboio"])
+        bomba = st.text_input("Bomba (Opcional)", value=st.session_state["bomba"])
+        codigo_material = None
+
+    if fonte_dados == "Estoque":
+        comboio = ""
+        bomba = ""
+        opcoes_material = {
+            "Diesel": "637577",
+            "ARLA": "637578",     
+        }
+        material_selecionado = st.selectbox("Material", options=list(opcoes_material.keys()))
+        codigo_material = opcoes_material[material_selecionado]
+
+
+        
 
     if st.button("Salvar Filtros"):
         st.session_state["data_ini"] = data_ini
         st.session_state["data_fim"] = data_fim
         st.session_state["comboio"] = comboio
+        st.session_state["bomba"] = bomba
         st.success("Filtros salvos na sessão.")
 
 # Consulta
@@ -46,21 +69,41 @@ if st.button("Consultar Dados"):
     with st.spinner("Consultando base ..."):
         try:
             conn = get_connection()
-            query = """
-                SELECT data, hora, frota, quantidade, motorista,
-                       frentista, odometro, horimetro,
-                       encerrante, bomba, comboio, log_integracao
-                FROM usa.ggabastecimentos
-                WHERE data BETWEEN %s AND %s
-            """
 
-            params = [data_ini, data_fim]
+            # Opção A - Abastecimento
+            if fonte_dados == "Abastecimento":
 
-            if comboio.strip():
-                query += " AND comboio = %s"
-                params.append(comboio.strip())
+                query = """
+                    SELECT data, hora, frota, quantidade, motorista,
+                        frentista, odometro, horimetro,
+                        encerrante, bomba, comboio,qt_arla,
+                            integrado, log_integracao
+                    FROM usa.ggabastecimentos
+                    WHERE data BETWEEN %s AND %s
+                """
 
-            query += " ORDER BY data DESC, hora DESC"
+                params = [data_ini, data_fim]
+
+                if comboio.strip():
+                    query += " AND comboio = %s"
+                    params.append(comboio.strip())
+
+                if bomba.strip():
+                    query += " AND bomba = %s"
+                    params.append(bomba.strip())
+
+                query += " ORDER BY data DESC, hora DESC"
+            
+            elif fonte_dados == "Estoque":
+                query = '''
+                SELECT deposito, "data", quantidade
+                FROM usa.estoque
+                WHERE material = %s
+                    AND "data" BETWEEN %s AND %s
+                ORDER BY "data" DESC
+            '''
+                params = [codigo_material, data_ini, data_fim]
+
 
             df = pd.read_sql(query, conn, params=params)
             conn.close()
@@ -72,6 +115,7 @@ if st.button("Consultar Dados"):
             else:
                 st.success(f"{len(df)} registros encontrados.")
                 st.dataframe(df)
+
                 if "quantidade" in df.columns:
                     try:
                         total_quant = pd.to_numeric(df["quantidade"], errors="coerce").sum()
